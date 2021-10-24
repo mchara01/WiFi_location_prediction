c45909e4c1279339aa1e17ae1f62258123bd902a
Wei Jie Chua <wjc354@nyu.edu>
2021-10-24 21:20:44 +0100

    added read_full_dt and main_method

--- evaluation.py
+++ evaluation.py
@@ -94,4 +94,47 @@ def evaluate(test_set, trained_tree):
         return 0.
 
 
-  
+
+def confusion_matrix(y_truth, y_prediction, class_labels=None):
+    """ Compute the confusion matrix.
+        
+    Args:
+        y_truth (np.ndarray): the correct ground truth/gold standard labels
+        y_prediction (np.ndarray): the predicted labels
+        class_labels (np.ndarray): a list of unique class labels. 
+                               Defaults to the union of y_gold and y_prediction.
+
+    Returns:
+        np.array : shape (C, C), where C is the number of classes. 
+                   Rows are ground truth per class, columns are predictions
+    """
+
+    # if no class_labels are given, we obtain the set of unique class labels from
+    # the union of the ground truth annotation and the prediction
+    if not class_labels:
+        class_labels = np.unique(np.concatenate((y_truth, y_prediction)))
+
+    confusion = np.zeros((len(class_labels), len(class_labels)), dtype=np.int)
+
+    # for each correct class (row), 
+    # compute how many instances are predicted for each class (columns)
+    for (i, label) in enumerate(class_labels):
+        # get predictions where the ground truth is the current class label
+        indices = (y_truth == label)
+        truth = y_truth[indices]
+        predictions = y_prediction[indices]
+
+        # quick way to get the counts per label
+        (unique_labels, counts) = np.unique(predictions, return_counts=True)
+
+        # convert the counts to a dictionary
+        frequency_dict = dict(zip(unique_labels, counts))
+
+        # fill up the confusion matrix for the current row
+        for (j, class_label) in enumerate(class_labels):
+            confusion[i, j] = frequency_dict.get(class_label, 0)
+
+    
+        
+
+    return confusion
--- init.py
+++ init.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from dataset import dataset
 from decision_tree import decision_tree
+from evaluation import *
 
 # CONSTANTS DECLARATION
 clean_dataset_file_path = "WIFI_db/clean_dataset.txt"
@@ -25,8 +26,68 @@ def read_dt(file_path, test_proportion, validation_proportion):
 
     return x_train, x_test, x_val, y_train, y_test, y_val, classes
 
+def read_full_dt(file_path, test_proportion, validation_proportion):
+    """This function reads the dataset from a given file path, splits it into training and test samples and returns the result along with their classes.
+
+    Args:
+        file_path ([String]): Location of the file on local storage
+        test_proportion ([float]): Percentage of testing samples in the dataset
+
+    Returns:
+        np.array, np.array: Results of the dataset spliting into X and Y and classes
+    """
+    x, y, classes = dataset.read_dataset(file_path)
+    
+
+    return x, y, classes
+
+
+def main_method():
+    testing_proportion = 0.3
+    validation_proportion = 0.5 
+
+
+    #  reading clean dataset and split into training and testing
+    x_train_clean, x_test_clean, x_val_clean, y_train_clean, y_test_clean, y_val_clean, classes_clean = None, None, None, None, None, None, None
+    if os.path.isfile(clean_dataset_file_path):
+        x, y, classes = read_full_dt(clean_dataset_file_path, testing_proportion, validation_proportion)
+    
+    else:
+        print("File 1 does not exist.")
+        print("Exiting...")
+        exit(0)
+        
+    acc_list = []
+    indices_list = nested_k_fold_cross_validation(10, len(x))
+    for k in range(len(indices_list)):
+        print("K:" + str(k))
+        inner_list = []
+        for j in range(len(indices_list[k])):
+            print("J:" + str(j))
+            train_indices = indices_list[k][j][0]
+            test_indices = indices_list[k][j][1]
+            val_indices = indices_list[k][j][2]
+            x_train = x[train_indices]
+            y_train = y[train_indices]
+
+            x_test = x[test_indices]
+            y_test = y[test_indices]
+            test_clean = np.concatenate((x_test, y_test.reshape(len(y_test),1)), axis=1)
+
+            clean_decision_tree = decision_tree()
+            clean_data_tree , clean_data_depth = clean_decision_tree.train(x_train,y_train)
+            #y_pred = clean_decision_tree.predict(x_test)
+            acc = evaluate(test_clean, clean_decision_tree)
+            print("Acc: " + str(acc))
+            inner_list.append(acc)
+        acc_list.append(inner_list)
+        
+    return acc_list
+
 
 if __name__ == "__main__":
+    #acc_list = main_method()
+
     # calculation as follows: split first by testing proportion, and then by validation proportion
     # for eg: if testing_proportion = 0.3, validation_proportion = 0.5, then train:test:val = 0.7:0.15:0.15
     testing_proportion = 0.3
@@ -58,3 +119,6 @@ if __name__ == "__main__":
     noise_decision_tree = decision_tree()
     clean_data_tree , clean_data_depth = clean_decision_tree.train(x_train_clean,y_train_clean)
     noise_data_tree , noise_data_depth = noise_decision_tree.train(x_train_noise,y_train_noise)
+
+    y_pred_clean = clean_decision_tree.predict(x_test_clean)
+    y_pred_noise = clean_decision_tree.predict(x_test_noise)
